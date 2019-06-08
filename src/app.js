363dca7593a304ea4848a127a26e8348bc8629ac
Debbie Ellis <debbieellis7@gmail.com>
2019-06-08 22:03:27 +0800

    Get & Display Posts

--- src/app.js
+++ src/app.js
@@ -1,2 +1,11 @@
-const greeting = 'Hello World'
-console.log(greeting)
+import { http } from './http';
+import { ui } from './ui';
+
+// Get posts on DOM load
+document.addEventListener('DOMContentLoaded', getPosts);
+
+function getPosts() {
+ http.get('http://localhost:3000/posts')
+  .then(data => ui.showPosts(data))
+  .catch(err => console.log(error));
+}
