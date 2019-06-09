9734e51271bfd23f7d25336c59d4be4e79c5c708
Debbie Ellis <debbieellis7@gmail.com>
2019-06-09 08:22:31 +0800

    Removing Posts

--- src/app.js
+++ src/app.js
@@ -7,6 +7,10 @@ document.addEventListener('DOMContentLoaded', getPosts);
 // Listen for add post
 document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+// Listen for delete
+document.querySelector('#posts').addEventListener('click', deletePost);
+
+// Get Posts
 function getPosts() {
  http.get('http://localhost:3000/posts')
   .then(data => ui.showPosts(data))
@@ -31,4 +35,22 @@ function submitPost() {
       getPosts();
     })
     .catch(err => console.log(err));
+}
+
+
+// Delete Post
+function deletePost(e) {
+  e.preventDefault();
+  
+  if(e.target.parentElement.classList.contains('delete')) {
+    const id = e.target.parentElement.dataset.id;
+    if(confirm('Are you sure?')) {
+      http.delete(`http://localhost:3000/posts/${id}`)
+        .then(data => {
+          ui.showAlert('Post Removed', 'alert alert-success');
+          getPosts();
+        })
+        .catch(err => console.log(err));
+    }
+  }
 }
